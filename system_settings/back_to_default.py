fd98a69393f983b742722e4a2c143f4052ea136f
ukolov-pavel <33089448+ukolov-pavel@users.noreply.github.com>
2018-04-19 18:49:15 +0300

    Add files via upload

--- system_settings/back_to_default.py
+++ system_settings/back_to_default.py
@@ -6,67 +6,20 @@ from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 import codecs
 
-payload = {'Login':BaseTest.login, 'Password':BaseTest.password}
-
-url = BaseTest.stand+'/Auth/LogOn'
-
-s = requests.Session()
-
-s.post(url, data=payload)
-
-constant_headers = {'Referer': BaseTest.stand+'/Admin/Setting/EditDonor', 'Content-Type': 'application/x-www-form-urlencoded'}
-
-cookies = s.cookies.get_dict()
-
-headers = {**constant_headers, **cookies}
-
-'''t = s.get(BaseTest.stand+'/Admin/Setting/EditDonor', headers=headers)
-
-soup = BeautifulSoup(t.text, 'html.parser')
-
-for script in soup.findAll('script'):
-	if script.get('src') == '/bundles/Setting/EditDonor?v=k3PZb1rGL2OJptx7_Wq4Yu8kZSWVRVievzK9AFvF6b01':
-		edit_donor = script
-	else:
-		pass
-
-settings = []
-
-for sett in str(edit_donor.findNextSibling()).split('\r\n'):
-	try:
-		if sett[16:22] == 'model.':
-			settings.append(sett[22:])
-	except:
-		pass
-
-dic = {}
-
-for i in settings:
-	if i.startswith('HonorableDonorSettings') == True:
-		i = i.replace(i[22], '[', 1)
-		i = i.replace('__', '].')
-		i = i.replace(i.split('=')[0], quote_plus(i.split('=')[0]))
-	dic[i.split('=')[0][:-1]] = i.split('=')[1][1:-1]
-
-for i in dic:
-	dic[i] = dic[i].strip("'")
-
-dic['DonorSubscription'] = quote_plus(dic['DonorSubscription'].replace('&quot;', '"').replace('\\', '\r'+'\\').replace('\\n', codecs.decode('\\n', 'unicode_escape')))
-
-dic['ViewTabName'] = 'EditDonor'
-
-dic['NextTabName'] = ''
-
-for k, v in dic.items():
-	dic[k] = v.strip(' ')'''
-
 
 class GetCurrentSettings(object):
-	def __init__(self, url, headers, script):
+
+	def __init__(self, url, script):
 		self.url = url
 		self.session = Session.get()
-		self.headers = headers
+		self.headers = {'Referer': BaseTest.stand+self.url, 'Content-Type': 'application/x-www-form-urlencoded'}
 		self.script = script
+		payload = {'Login': BaseTest.login, 'Password': BaseTest.password}
+		login_url = BaseTest.stand + '/Auth/LogOn'
+		self.session.post(url=login_url, data=payload)
+		self.cookies = self.session.cookies.get_dict()
+		self.headers = {**self.headers, **self.cookies}
+
 	def get_raw_settings(self):
 		t = self.session.get(BaseTest.stand+self.url, headers=self.headers)
 		soup = BeautifulSoup(t.text, 'html.parser')
@@ -84,6 +37,7 @@ class GetCurrentSettings(object):
 			except:
 				pass
 		return settings
+
 	def get_settings(self):
 		dic = {}
 		for i in self.get_raw_settings():
@@ -91,16 +45,20 @@ class GetCurrentSettings(object):
 		for i in dic:
 			dic[i] = dic[i].strip("'")
 		return dic
+
 	def post_settings(self, settings_dic):
 		request_body_list = []
 		for i in settings_dic:
 			request_body_list.append(i + '=' + settings_dic[i])
-		request_body = '&'.join(request_body_list)
-		return self.session.post(BaseTest.stand + self.url, data=request_body, headers=headers)
+		self.request_body = '&'.join(request_body_list)
+		return self.session.post(BaseTest.stand + self.url, data=self.request_body, headers=self.headers)
+
 
 class TurnBackDonorSettings(GetCurrentSettings):
-	def __init__(self, session, headers, script='k3PZb1rGL2OJptx7_Wq4Yu8kZSWVRVievzK9AFvF6b01', url='/Admin/Setting/EditDonor'):
-		GetCurrentSettings.__init__(self, url, session, headers, script)
+
+	def __init__(self, script='k3PZb1rGL2OJptx7_Wq4Yu8kZSWVRVievzK9AFvF6b01', url='/Admin/Setting/EditDonor'):
+		GetCurrentSettings.__init__(self, url, script)
+
 	def get_settings(self):
 		settings = self.get_raw_settings()
 		dic = {}
