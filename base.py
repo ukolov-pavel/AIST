b8d59313598489f08f8330e6e590fedbc860a21f
ukolov-pavel <33089448+ukolov-pavel@users.noreply.github.com>
2018-05-18 18:01:15 +0300

    Add files via upload

--- base.py
+++ base.py
@@ -23,22 +23,12 @@ class Session(object):
         return cls.__instance
 
 class BaseTest(object):
-    '''
-    Реализована работа с настройками системы:
-    при создании тестов, в которых используются те или иные глобальные настройки, для возврата к исходным
-    состояниям настроек, затрагиваемых автотестом, отныне нужно присваивать переменной used_settings
-    список классов из back_to_default.py, в соответствии с настройками, которые нужны в тесте.
-    '''
-    stand = "http://10.32.200.142"
+    stand = 'http://10.32.200.142'
     login = 'autotest'
     password = '77@dm1nA'
     timeout = 15
-    used_settings_classes = []
-    started_settings = []
     @classmethod
     def setup_class(cls):
-        for setting in cls.used_settings_classes:
-            cls.started_settings.append(setting.get_settings())
         cls.driver = Driver.get()
         #cls.driver.implicitly_wait(0)
         cls.driver.maximize_window()
@@ -50,8 +40,4 @@ class BaseTest(object):
     @classmethod
     def teardown_class(cls):
         Driver.get().close()
-        if cls.started_settings:
-            for dic in cls.started_settings:
-                if dic:
-                    cls.used_settings_classes[cls.started_settings.index(dic)]().post_settings(cls.started_settings[cls.started_settings.index(dic)])
-            Session.get().close()
+        Session.get().close()
